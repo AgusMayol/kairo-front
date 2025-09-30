a915083f20d9d724c407982e847e3d7891204488
Agustin Mayol <agusmayolitos@gmail.com>
2025-09-29 22:39:08 -0300

    Añadiendo redireccionamiento a login y recovery page. Mejoras en estilos.

--- src/app/password-recovery-component/password-recovery-component.ts
+++ src/app/password-recovery-component/password-recovery-component.ts
@@ -8,5 +8,40 @@ import {  } from '@angular/router';
   styleUrl: './password-recovery-component.css'
 })
 export class PasswordRecoveryComponent {
+  isPasswordVisible: boolean = false;
+  isConfirmPasswordVisible: boolean = false;
+  
+  showPassword(event: Event): void {
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    if (passwordInput) {
+      passwordInput.type = 'text';
+      this.isPasswordVisible = true;
+    }
+  }
 
+  hidePassword(event: Event): void {
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    if (passwordInput) {
+      passwordInput.type = 'password';
+      this.isPasswordVisible = false;
+    }
+  }
+
+
+
+  showConfirmPassword(event: Event): void {
+    const passwordInput = document.getElementById('confirmPassword') as HTMLInputElement;
+    if (passwordInput) {
+      passwordInput.type = 'text';
+      this.isConfirmPasswordVisible = true;
+    }
+  }
+
+  hideConfirmPassword(event: Event): void {
+    const passwordInput = document.getElementById('confirmPassword') as HTMLInputElement;
+    if (passwordInput) {
+      passwordInput.type = 'password';
+      this.isConfirmPasswordVisible = false;
+    }
+  }
 }
